5fa20a22c7d800f644001c0f9fee4517b0391b5d
Sanjaykumar-swfy <Sanjay.kumar@swyftbooking.com>
2025-04-18 22:15:21 +0530

    gs

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -199,6 +199,18 @@ exports.socialLogin = catchAsync(async (req, res, next) => {
             device_token
         });
     }
+    else {
+        let updated = false;
+        if (device_token && user.device_token !== device_token) {
+            user.device_token = device_token;
+            updated = true;
+        }
+        if (device_type && user.device_type !== device_type) {
+            user.device_type = device_type;
+            updated = true;
+        }
+        if (updated) await user.save();
+    }
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN,
     });
